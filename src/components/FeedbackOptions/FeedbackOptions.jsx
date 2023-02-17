f956abbd2160c4265b65629136ea8eea9e0c1433
marynazakvatska <maryna.zakvatska@gmail.com>
2023-02-17 12:13:39 +0100

    feedback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,15 +1,4 @@
-/*  const FeedbackOptions = ({goodIncrement, neutralIncrement, badIncrement}) => {
-    return (
-         <div>
-                 <button type='button' onClick={goodIncrement}>Good</button>
-            <button type='button' onClick={neutralIncrement}>Neutral</button>
-                     <button type='button' onClick={badIncrement}>Bad</button>
-                     </div>
-    )
-} 
-export default FeedbackOptions
- */
-{/* <FeedbackOptions options={} onLeaveFeedback={}> */ }
+
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return Object.keys(options).map(btnName => {
